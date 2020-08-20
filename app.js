6c67b698ae70d48d75e89cca8d6e15e4e0b28d79
Divyansh Tripathi <3151-divyansh.t.aux@users.noreply.source.golabs.io>
2020-08-20 12:06:26 +0530

    Backend completed

--- app.js
+++ app.js
@@ -28,55 +28,75 @@ app.get("/candidates", (req, res) => {
 app.get("/interview", (req, res) => {
   connection.query("SELECT * FROM sql12361367.INTERVIEW", function (
     error,
-    results,
-    fields
+    results
   ) {
     if (error) {
       res.status(500).json(error);
     } else {
-      res.status(200).json(results[0]["1"]);
+      res.status(200).json(results);
     }
   });
 });
 
+// Creates new interview record with all the validation
 app.post("/interview", (req, res) => {
   connection.query(
     `SELECT candidate_id FROM 
-	(SELECT candidate_id,schedule_id,start_time,end_time FROM sql12361367.SCHEDULE INNER JOIN sql12361367.INTERVIEW ON sql12361367.INTERVIEW.interview_id=sql12361367.SCHEDULE.interview_id) AS T
-	WHERE T.start_time>1 AND T.end_time<5;`,
-    function (error, results, field) {
-      connection.query(
-        `SELECT COUNT(*) FROM sql12361367.INTERVIEW WHERE start_time>1 AND end_time>1;`,
-        function (error, results, field) {
-          connection.query(
-            `INSERT INTO sql12361367.INTERVIEW (start_time, end_time, user_id) 
-    values(${req.body.start_time},${req.body.end_time},${req.body.user_id});`,
-            function (error, results, fields) {
-              if (error) {
-                res.status(500).json(error);
-              } else {
-                var interview_id = 1;
-                var sql =
-                  "INSERT INTO SCHEDULE (candidate_id, interview_id) VALUES ?";
-                var values = [
-                  [2, interview_id],
-                  [3, interview_id],
-                ];
+        (SELECT candidate_id,schedule_id,start_time,end_time 
+            FROM sql12361367.SCHEDULE 
+            INNER JOIN sql12361367.INTERVIEW ON sql12361367.INTERVIEW.interview_id=sql12361367.SCHEDULE.interview_id) AS T
+        WHERE (T.start_time>${req.body.start_time} AND T.start_time<${req.body.end_time}) 
+            OR (T.end_time>${req.body.start_time} AND T.end_time<${req.body.end_time}) ;`,
 
-                conn.query(sql, [values], function (err) {
-                  if (err) throw err;
-                  conn.end();
-                });
-                res.status(200).json(results[0]["1"]);
+    function (error, results) {
+      if (error) {
+        res.status(500).json(error);
+      }
+      if (results.length == 0) {
+        connection.query(
+          `SELECT * FROM sql12361367.INTERVIEW
+            WHERE (T.start_time>${req.body.start_time} AND T.start_time<${req.body.end_time}) 
+            OR (T.end_time>${req.body.start_time} AND T.end_time<${req.body.end_time}) ;`,
+
+          function (error, results) {
+            if (!error) {
+              if (results.length == 0) {
+                connection.query(
+                  `INSERT INTO sql12361367.INTERVIEW (start_time, end_time, user_id) 
+                    values(${req.body.start_time},${req.body.end_time},${req.body.user_id});`,
+                  function (error, results) {
+                    if (error) {
+                      res.status(500).json(error);
+                    } else {
+                      var interview_id = 1;
+                      var sql =
+                        "INSERT INTO SCHEDULE (candidate_id, interview_id) VALUES ?";
+                      var values = [
+                        [2, interview_id],
+                        [3, interview_id],
+                      ];
+
+                      conn.query(sql, [values], function (err) {
+                        if (err) throw err;
+                        conn.end();
+                      });
+                      res.status(200).json(results);
+                    }
+                  }
+                );
               }
+            } else {
             }
-          );
-        }
-      );
+          }
+        );
+      } else {
+        res.status(400).json(result);
+      }
     }
   );
 });
 
+// Updates interview time
 app.patch("/interview/:id", (req, res) => {
   connection.query(
     `UPDATE sql12361367.INTERVIEW
@@ -92,6 +112,7 @@ app.patch("/interview/:id", (req, res) => {
   );
 });
 
+// Deletes Interview
 app.delete("/interview/:id", (req, res) => {
   connection.query(
     "DELETE FROM sql12361367.INTERVIEW WHERE interview_id=" + req.params.id,
