d26f7f1cd5f0dacf7a6132edef1b64a54c01bced
Divyansh Tripathi <3151-divyansh.t.aux@users.noreply.source.golabs.io>
2020-08-20 11:00:57 +0530

    SQL queries sorted

--- app.js
+++ app.js
@@ -41,14 +41,38 @@ app.get("/interview", (req, res) => {
 
 app.post("/interview", (req, res) => {
   connection.query(
-    `INSERT INTO sql12361367.INTERVIEW (start_time, end_time, user_id) 
+    `SELECT candidate_id FROM 
+	(SELECT candidate_id,schedule_id,start_time,end_time FROM sql12361367.SCHEDULE INNER JOIN sql12361367.INTERVIEW ON sql12361367.INTERVIEW.interview_id=sql12361367.SCHEDULE.interview_id) AS T
+	WHERE T.start_time>1 AND T.end_time<5;`,
+    function (error, results, field) {
+      connection.query(
+        `SELECT COUNT(*) FROM sql12361367.INTERVIEW WHERE start_time>1 AND end_time>1;`,
+        function (error, results, field) {
+          connection.query(
+            `INSERT INTO sql12361367.INTERVIEW (start_time, end_time, user_id) 
     values(${req.body.start_time},${req.body.end_time},${req.body.user_id});`,
-    function (error, results, fields) {
-      if (error) {
-        res.status(500).json(error);
-      } else {
-        res.status(200).json(results[0]["1"]);
-      }
+            function (error, results, fields) {
+              if (error) {
+                res.status(500).json(error);
+              } else {
+                var interview_id = 1;
+                var sql =
+                  "INSERT INTO SCHEDULE (candidate_id, interview_id) VALUES ?";
+                var values = [
+                  [2, interview_id],
+                  [3, interview_id],
+                ];
+
+                conn.query(sql, [values], function (err) {
+                  if (err) throw err;
+                  conn.end();
+                });
+                res.status(200).json(results[0]["1"]);
+              }
+            }
+          );
+        }
+      );
     }
   );
 });
